6880f0ae7b16c1fc655f3946408718176e304c34
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2024-01-20 19:44:21 -0800

    worked on the form

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Form() {
 	return (
-		<form action="">
-			<label htmlFor=""></label>
+		<form className="form-container" action ="">
+			<label htmlFor="top-text">Top text</label><br />
+			<input type="text" id="top-text" />
+			<label htmlFor="bot-text">Bottom text</label><br />
+			<input type="text" id="bot-text" /><br />
+			<input type="submit" value="Get a new meme image 🖼" />
 		</form>
 	)
 }
