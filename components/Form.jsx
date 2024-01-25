1c0fb7dde2866eb3ef7ae25d909575ed9e14ea5a
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2024-01-24 19:15:02 -0800

    redid form with flex to grid instead

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,19 +2,13 @@ import React from 'react'
 
 function Form() {
 	return (
-		<form className="form-container" action ="">
-			<div className="form-row">
-                <div>
-                    <label htmlFor="top-text">Top text</label><br />
-                    <input className="input" placeholder="Type here" type="text" id="top-text" />
-                </div>
-                <div>
-                    <label htmlFor="bot-text">Bottom text</label><br />
-                    <input className="input" placeholder="Type here" type="text" id="bot-text" /><br />
-                </div>
-            </div>
-			<input className="button" type="submit" value="Get a new meme image" />
-		</form>
+		<main>
+			<form action="">
+				<input className="form-row" type="text" placeholder= "Type here" id="top-text" />
+				<input className="form-row" type="text" placeholder= "Type here" id="bot-text" />
+				<input className="button" type="submit" value="Get a new meme image" />
+			</form>
+		</main>
 	)
 }
 
